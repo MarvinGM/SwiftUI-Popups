1e14b68a89e5ae45c6f245ce638b4b3e49eaa738
MarvinGM <marvinmahilum091197@gmail.com>
2021-06-15 18:27:38 +0800

    Initial Commit

---
+++ SwiftUI Popups/SwiftUI_PopupsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_PopupsApp.swift
+//  SwiftUI Popups
+//
+//  Created by Amben on 6/15/21.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_PopupsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
